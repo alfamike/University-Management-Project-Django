c309919c2a426396e3c03f4db37c3a42a9bab168
Álvaro Menacho Rodríguez <alfamike.dev@gmail.com>
2024-11-17 18:04:47 +0100

    Added: - Static files

--- university_management/university_management/settings.py
+++ university_management/university_management/settings.py
@@ -113,10 +113,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
