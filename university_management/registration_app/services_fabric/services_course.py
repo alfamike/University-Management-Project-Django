2b520a2305e48b542a98edfb941a1da1bafed083
Álvaro Menacho Rodríguez <alfamike.dev@gmail.com>
2024-11-17 21:45:30 +0100

    Added: - Method to query courses by title

--- university_management/registration_app/services_fabric/services_course.py
+++ university_management/registration_app/services_fabric/services_course.py
@@ -1,3 +1,4 @@
+from registration_app.models import Title, Course
 from registration_app.services_fabric.services_fabric import get_fabric_client
 
 
@@ -87,4 +88,23 @@ def get_all_courses():
     )
 
     # Retornar la respuesta (debe ser una lista de cursos en formato JSON o similar)
+    return response
+
+
+def get_courses_by_title(title_id):
+    fabric_client = get_fabric_client()
+
+    # Cargar el canal y la identidad del usuario
+    channel = fabric_client.get_channel('mychannel')
+    admin_user = fabric_client.get_user('Org1', 'Admin')
+
+    # Consultar el Chaincode para obtener todos los cursos
+    response = channel.chaincode_query(
+        requestor=admin_user,
+        channel_name='mychannel',
+        chaincode_name='mycc',  # Nombre de tu Chaincode
+        fcn='GetCoursesByTitle',  # Función en el Chaincode
+        args=[title_id]
+    )
+
     return response
