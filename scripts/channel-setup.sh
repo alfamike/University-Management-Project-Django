15c53e9ba509da362e50e8da30db5298f35ed303
Álvaro Menacho Rodríguez <alfamike.dev@gmail.com>
2025-01-11 22:38:00 +0100

    On fly channel setup

--- scripts/channel-setup.sh
+++ scripts/channel-setup.sh
@@ -5,16 +5,16 @@ sleep 10
 
 # Create the channel
 echo "Creating the channel..."
-peer channel create -o orderer.university.eu:7050 -c registration-channel -f /etc/hyperledger/fabric/channel-artifacts/registration-channel.tx --tls --cafile /etc/hyperledger/fabric/crypto-config/ordererOrganizations/university.eu/tlsca/tlsca.university.eu-cert.pem --clientauth --keyfile /etc/hyperledger/fabric/crypto-config/peerOrganizations/org1.university.eu/users/Admin@org1.university.eu/msp/keystore/priv_sk  --certfile /etc/hyperledger/fabric/crypto-config/peerOrganizations/org1.university.eu/users/Admin@org1.university.eu/msp/signcerts/Admin@org1.university.eu-cert.pem 
+peer channel create -o orderer.university.eu:7050 -c registration-channel -f /etc/hyperledger/fabric/channel-artifacts/registration-channel.tx --tls --cafile /etc/hyperledger/fabric/crypto-config/ordererOrganizations/university.eu/tlsca/tlsca.university.eu-cert.pem --clientauth --keyfile /etc/hyperledger/fabric/crypto-config/peerOrganizations/org1.university.eu/users/Admin@org1.university.eu/msp/keystore/priv_sk  --certfile /etc/hyperledger/fabric/crypto-config/peerOrganizations/org1.university.eu/users/Admin@org1.university.eu/msp/signcerts/Admin@org1.university.eu-cert.pem -outputBlock /etc/hyperledger/fabric/registration-channel.block
 
 # Join the peer0 to the channel
 echo "Peer0 joining the channel..."
-peer channel join -b registration-channel.block --tls --cafile /etc/hyperledger/fabric/crypto-config/ordererOrganizations/university.eu/tlsca/tlsca.university.eu-cert.pem --clientauth --keyfile /etc/hyperledger/fabric/crypto-config/peerOrganizations/org1.university.eu/users/Admin@org1.university.eu/msp/keystore/priv_sk  --certfile /etc/hyperledger/fabric/crypto-config/peerOrganizations/org1.university.eu/users/Admin@org1.university.eu/msp/signcerts/Admin@org1.university.eu-cert.pem 
+peer channel join -b /etc/hyperledger/fabric/registration-channel.block --tls --cafile /etc/hyperledger/fabric/crypto-config/ordererOrganizations/university.eu/tlsca/tlsca.university.eu-cert.pem --clientauth --keyfile /etc/hyperledger/fabric/crypto-config/peerOrganizations/org1.university.eu/users/Admin@org1.university.eu/msp/keystore/priv_sk  --certfile /etc/hyperledger/fabric/crypto-config/peerOrganizations/org1.university.eu/users/Admin@org1.university.eu/msp/signcerts/Admin@org1.university.eu-cert.pem
 
 # Join the peer1 to the channel
 echo "Peer1 joining the channel..."
 export CORE_PEER_ADDRESS=peer1.org1.university.eu:8051
-peer channel join -b registration-channel.block --tls --cafile /etc/hyperledger/fabric/crypto-config/ordererOrganizations/university.eu/tlsca/tlsca.university.eu-cert.pem --clientauth --keyfile /etc/hyperledger/fabric/crypto-config/peerOrganizations/org1.university.eu/users/Admin@org1.university.eu/msp/keystore/priv_sk  --certfile /etc/hyperledger/fabric/crypto-config/peerOrganizations/org1.university.eu/users/Admin@org1.university.eu/msp/signcerts/Admin@org1.university.eu-cert.pem 
+peer channel join -b /etc/hyperledger/fabric/registration-channel.block --tls --cafile /etc/hyperledger/fabric/crypto-config/ordererOrganizations/university.eu/tlsca/tlsca.university.eu-cert.pem --clientauth --keyfile /etc/hyperledger/fabric/crypto-config/peerOrganizations/org1.university.eu/users/Admin@org1.university.eu/msp/keystore/priv_sk  --certfile /etc/hyperledger/fabric/crypto-config/peerOrganizations/org1.university.eu/users/Admin@org1.university.eu/msp/signcerts/Admin@org1.university.eu-cert.pem
 
 # Setup completed
 echo "Channel setup completed!"
